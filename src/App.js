e5c237e7cb372ec00d09cd9c9ad1d4f2a59ab700
CheeEun <cheeeunahn@gmail.com>
2021-11-20 08:41:01 +0900

    delete unused files and add icons
    - deleted unused files from example code
    - changed the structure of the folders
    - installed react-icons

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import logo from './profiledrawing-512px.png';
 import './App.css';
 import { ReactP5Wrapper } from "react-p5-wrapper";
-import sketch from './Sketch.js'
+import Sketch from './sketches/Sketch.js'
+import Header from './components/Header.js'
 
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <div className="App-header">
+      <Header />
+      <div className="App-content">
         <p>
           Innovating the User Experience
         </p>
         <img className= "App-logo" src={logo} alt="Logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </div>
       <div className="Sketch">
-        <ReactP5Wrapper sketch={sketch} />
+        <ReactP5Wrapper sketch={Sketch} />
       </div>
     </div>
   );

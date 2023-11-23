c156653c61ee595f4d542de2e5ec05beff1d78fd
CheeEun <cheeeunahn@gmail.com>
2023-11-24 06:41:45 +0900

    adding more images

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -5,6 +5,9 @@ import 'react-slideshow-image/dist/styles.css';
 import image1 from "../images/1.jpg"
 import image2 from "../images/2.jpg"
 import image3 from "../images/3.jpg"
+import image4 from "../images/4.jpg"
+import image5 from "../images/5.jpg"
+import image6 from "../images/6.jpg"
 
 {/*https://react-slideshow-image.netlify.app/?path=/story/introduction--page*/}
 
@@ -22,10 +25,16 @@ const Slideshow = () => {
                 <img className="slide-image" src={image2} alt='main_image' width={'70%'} height={'auto'}/>
             </div>
             <div className="each-slide-effect">
-                <img className="slide-image" src={image3} alt='main_image' width={'70%'} height={'auto'}/>
+                <img className="slide-image" src={image5} alt='main_image' width={'70%'} height={'auto'}/>
             </div>
             <div className="each-slide-effect">
                 <img className="slide-image" src={image1} alt='main_image' width={'70%'} height={'auto'}/>
+            </div>
+            <div className="each-slide-effect">
+                <img className="slide-image" src={image4} alt='main_image' width={'70%'} height={'auto'}/>
+            </div>
+            <div className="each-slide-effect">
+                <img className="slide-image" src={image3} alt='main_image' width={'70%'} height={'auto'}/>
             </div>
         </Slide>
     );
